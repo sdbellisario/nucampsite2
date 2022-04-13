928a7937ba9fb56cf444668fda5a0384b8d9ec60
Sean Dufty <seandufty@gmail.com>
2022-04-13 10:00:40 -0700

    Header and Footer

---
+++ src/components/MainComponent.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Directory from "./DirectoryComponent";
+import { CAMPSITES } from "../shared/campsites";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import CampsiteInfo from './CampsiteInfoComponent';
+
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsite: CAMPSITES,
+            selectedCampsite: null
+        };
+    }
+
+    onCampsiteSelect(campsiteId) {
+        this.setState({selectedCampsite: campsiteId});
+    }
+    
+    render() {
+        return (
+            <div>
+                <Header />
+                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
+                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Main;
