49af3424ca651173c68843ede1272d9d2e9c3412
Sean Dufty <seandufty@gmail.com>
2022-04-26 11:39:16 -0700

    Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem,  Button, Modal, ModalHeader, ModalBody, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from './LoadingComponent';
 
 function RenderCampsite({campsite}) {
     return (
@@ -15,7 +17,7 @@ function RenderCampsite({campsite}) {
     )
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if(comments) {
         return (
             <div className="col-md-5 m-1">
@@ -28,6 +30,7 @@ function RenderComments({comments}) {
                         </div>
                     );
                 })}
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -36,6 +39,26 @@ function RenderComments({comments}) {
 
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
         return (
             <div className="container">
@@ -51,12 +74,74 @@ function CampsiteInfo(props) {
             </div>
                 <div class="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
     }
     return <div />;
 }
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        }
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    }
+
+    toggleModal = () => {
+        this.setState ({
+            isModalOpen : !this.state.isModalOpen 
+        })
+    }
+
+    render() {
+        const { isModalOpen } = this.state
+        return (
+            <>
+                <Button outline onClick={this.toggleModal}>
+                    <i className="fa fa-pencil fa-lg" /> submit comment
+                </Button>
+                <Modal isOpen={isModalOpen}>
+                    <ModalHeader toggle={this.toggleModal}>submit yourself</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select className="form-control" modal=".rating" id="rating" name="rating">
+                                    <option value="">1</option>
+                                    <option value="">2</option>
+                                    <option value="">3</option>
+                                    <option value="">4</option>
+                                    <option value="">5</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author">your name here</Label>
+                                <Control.text className="form-control" model=".author" id="author" name="author" placeholder="enter your name"></Control.text>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text">comment</Label>
+                                <Control.textarea classname="form-control" model=".text" id="text" name="text" rows="6"></Control.textarea>
+                            </div>
+                            <Button color="primary" type="submit" value="submit">submit</Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </>
+        )
+    }
+}
+
 
 export default CampsiteInfo;
